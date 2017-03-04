f1788368d5c1e680f60dc0759ba4754a600cf405
Carles Climent Granell <carlescliment@hotmail.com>
2017-03-05 00:35:57 +0100

    Rename Measure to Measurement

--- compost/models.py
+++ compost/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Measure(models.Model):
+class Measurement(models.Model):
     temperature = models.DecimalField(max_digits=2, decimal_places=2)
     taken_at = models.DateTimeField(True)
