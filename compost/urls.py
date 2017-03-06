84246566403f2cff986af715005f5050c49992b0
Carles Climent Granell <carlescliment@hotmail.com>
2017-03-06 23:44:09 +0100

    Add a view for adding temperatures that does nothing

--- compost/urls.py
+++ compost/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from compost.views import home, add_measurement
+from compost.views import home, new_measurement, create_measurement
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^measurements/add/$', add_measurement),
-    url(r'^$', home),
+    url(r'^measurements/create/$', create_measurement, name='measurements_create'),
+    url(r'^measurements/new/$', new_measurement, name='measurements_new'),
+    url(r'^$', home, name='home'),
 ]
--- compost/views.py
+++ compost/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import datetime
 
 def home(request):
     now = datetime.datetime.now()
     return render(request, 'home.html', {'now': now})
 
-def add_measurement(request):
+def new_measurement(request):
     return render(request, 'measurements/add.html')
+
+def create_measurement(request):
+    return redirect('home')
