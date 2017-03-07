e84f7d4ffe84f06789aee5fd1ce221979a7231fc
Carles Climent Granell <carlescliment@hotmail.com>
2017-03-07 23:31:04 +0100

    Refactor to constant

--- compost/forms.py
+++ compost/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 
 class MeasurementForm(forms.Form):
+    ABSOLUTE_ZERO = -273.15
+
     temperature = forms.FloatField(required=True)
 
     def clean_temperature(self):
         temperature = self.cleaned_data['temperature']
-        if temperature < -273.15:
+        if temperature < self.ABSOLUTE_ZERO:
             raise forms.ValidationError("Temperature can't be under absolute zero")
         return temperature
