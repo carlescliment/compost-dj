ec6ac93beaf5e30f71d9986e0f92f57e6f9974f4
Carles Climent Granell <carlescliment@hotmail.com>
2017-03-07 23:28:39 +0100

    Temperature cannot be under absolute zero

--- compost/forms.py
+++ compost/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class MeasurementForm(forms.Form):
     temperature = forms.FloatField(required=True)
+
+    def clean_temperature(self):
+        temperature = self.cleaned_data['temperature']
+        if temperature < -273.15:
+            raise forms.ValidationError("Temperature can't be under absolute zero")
+        return temperature
--- compost/models.py
+++ compost/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Measurement(models.Model):
-    temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
     taken_at = models.DateTimeField(True)
